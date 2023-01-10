baa3fa9405439cc08aabf1987e57ab32c1c05f1b
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2023-01-10 20:32:53 +0100

    14.2 autowired test variable

--- kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
@@ -1,6 +1,7 @@
 package com.kodilla.spring.library;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -8,13 +9,12 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 @SpringBootTest
 public class LibraryTestSuite {
 
+    @Autowired
+    private Library library;
+
     @Test
     void testLoadFromDb() {
         //Given
-        ApplicationContext context =
-                new AnnotationConfigApplicationContext("com.kodilla.spring");
-        Library library = context.getBean(Library.class);
-
         //When
         library.loadFromDb();
 
@@ -25,10 +25,6 @@ public class LibraryTestSuite {
     @Test
     void testSaveToDb() {
         //Given
-        ApplicationContext context =
-                new AnnotationConfigApplicationContext("com.kodilla.spring");
-        Library library = context.getBean(Library.class);
-
         //When
         library.saveToDb();
 
